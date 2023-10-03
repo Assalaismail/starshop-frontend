212e46ae4815e0c1d8e2d6aaac1c9c226cdceb2a
assala <ismailassala1@gmail.com>
2023-10-03 16:56:35 +0300

    in progress

--- src/components/SingleProductSubCategory.vue
+++ src/components/SingleProductSubCategory.vue
@@ -1,44 +1,78 @@
 <template>
   <div class="single-page">
-
-  <div class="color-size">
-    <p class="firstName">{{ products.length > 0 ? products[0].name : 'No products available' }}</p>
-
-    <div v-if="selectedColor && selectedSize">
-      <p v-for="product in productsByColorAndSize(selectedColor, selectedSize)" :key="product.id">
-        ({{ product.stock_status }})
-      </p>
-    </div>
-
-    <div v-else>
-      <p class="default-instock">(In Stock)</p>
-    </div>
-
-
-    <select v-model="selectedColor" class="select-color">
-      <option value="" disabled>Select color</option>
-      <option v-for="color in uniqueColors" :key="color" :value="color">{{ color }}</option>
-    </select>
-    
-    <select v-model="selectedSize" class="select-size">
-      <option value="">Select size</option> 
-      <option v-for="size in sizesByColor(selectedColor)" :key="size">{{ size }}</option>
-    </select>
-
-  </div>
-
-
-    <div v-if="selectedColor">
-      <div v-for="product in productsByColor(selectedColor)" :key="product.id">
-        <div v-for="(image, index) in JSON.parse(product.images)" :key="index">
-          <img :src="image" alt="Product Image" class="single-image">
+    <div class="image-carousel">
+      <div class="thumbnail-container">
+        <div
+          v-for="(product, productIndex) in products"
+          :key="product.id"
+          class="thumbnail"
+          @click="showImage(productIndex)"
+        >
+          <img
+            :src="JSON.parse(product.images)[0]"
+            alt="Product Image"
+            class="small-image"
+          />
         </div>
+      </div>
+      <div class="big-image-container">
+        <img
+          :src="selectedImage"
+          alt="Product Image"
+          class="big-image"
+        />
       </div>
     </div>
 
-    <div v-else>
-      
+    <div class="column-2">
+
+    <div class="name-status">
+      <p class="firstName">
+        {{ products.length > 0 ? products[0].name : 'No products available' }}
+      </p>
+
+      <div v-if="selectedColor && selectedSize">
+        <p v-for="product in productsByColorAndSize(selectedColor, selectedSize)" :key="product.id" :class="{ 'text-green': product.stock_status === 'In Stock', 'text-red': product.stock_status === 'Out of Stock' }">
+           ({{ product.stock_status }})
+        </p>
+      </div>
+
+      <div v-else>
+        <p class="default-instock">(In Stock)</p>
+      </div>
+
+
     </div>
+
+    <div class="sku-price">
+    <p class="sku">
+       SKU: {{ products.length > 0 ? products[0].sku : 'No products available' }}
+    </p>
+
+    <p class="single-price">
+        ${{ products.length > 0 ? products[0].price : 'No products available' }}
+    </p>
+
+  </div>
+
+    <p>Color</p>
+      <select v-model="selectedColor" class="select-color">
+        <option value="" disabled>Select color</option>
+        <option v-for="color in uniqueColors" :key="color" :value="color">{{ color }}</option>
+      </select>
+
+      <p>Size</p>
+      <select v-model="selectedSize" class="select-size">
+        <option value="" disabled>Select size</option> 
+        <option v-for="size in sizesByColor(selectedColor)" :key="size">{{ size }}</option>
+      </select>
+
+
+     
+
+
+    </div>
+
   </div>
 </template>
 
@@ -53,6 +87,7 @@ export default {
       products: [],
       selectedColor: "", // To store the selected color
       selectedSize: "", // To store the selected size
+      selectedImage: null,
     };
   },
 
@@ -61,9 +96,24 @@ export default {
     this.fetchProducts(this.parentName);
   },
 
+  watch: {
+    // Watch for changes in products and set the default selected image
+    products: {
+      handler(newProducts) {
+        if (newProducts.length > 0) {
+          const firstProductImages = JSON.parse(newProducts[0].images);
+          this.selectedImage = firstProductImages[0];
+        } else {
+          this.selectedImage = null;
+        }
+      },
+      deep: true,
+      immediate: true,
+    },
+  },
+
   computed: {
     uniqueColors() {
-      // Use a Set to get unique colors
       const colorSet = new Set();
       this.products.forEach((product) => {
         colorSet.add(product.color);
@@ -78,7 +128,6 @@ export default {
         .get(`http://127.0.0.1:8000/api/childs/${parentName}`)
         .then((response) => {
           console.log("API Response:", response.data);
-          // Access the products array within the parentName object
           this.products = response.data.data[parentName];
         })
         .catch((error) => {
@@ -87,7 +136,6 @@ export default {
     },
 
     sizesByColor(color) {
-      // Filter products by color and get unique sizes
       const sizes = new Set();
       this.products.forEach((product) => {
         if (product.color === color) {
@@ -97,60 +145,124 @@ export default {
       return Array.from(sizes);
     },
 
-    productsByColor(color) {
-      // Filter products by color and size
-      return this.products.filter((product) => product.color === color);
-    },
-
     productsByColorAndSize(color, size) {
-      // Filter products by color and size
       return this.products.filter((product) => product.color === color && product.size === size);
     },
 
-    
+    showImage(productIndex) {
+      const product = this.products[productIndex];
+      const images = JSON.parse(product.images);
+      this.selectedImage = images[0];
+    },
+
+
   },
 };
 </script>
 
-
 <style scoped>
 .single-page {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr); /* Two columns: images and text */
-  grid-gap: 20px;
+  display: flex;
   justify-content: center;
+  gap: 100px;
 }
 
 /* Left side (images) */
 .single-image {
- width: 200px;
- height: 200px;
+  width: 200px;
+  height: 200px;
+  margin-right: 20px; 
+}
+
+.column-2{
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  padding-left: 10px;
 }
 
 /* Right side (text) */
-.color-size {
-  display: grid;
-  grid-template-columns: 1fr; /* One column for text */
-  grid-gap: 10px; /* Adjust the gap as needed */
+
+.firstName {
+  font-size: 30px;
+  text-transform: uppercase;
 }
-.firstName{
-  
+
+.name-status{
+border-bottom: 1px solid #737373;
 }
-.default-instock {
-  font-weight: bold;
-  font-size: 20px;
+
+.text-green {
+  text-align: start;
   color: green;
 }
 
-.select-color , .select-size {
+.text-red {
+  color: red;
+  text-align: start;
+
+}
+
+.sku-price{
+  text-align: start;
+
+}
+
+.single-price{
+  font-weight: 900;
+}
+
+.default-instock {
+  font-size: 16px;
+  color: green;
+  text-align: start;
+}
+
+.select-color,
+.select-size {
   width: 120px;
   height: 56px;
   border: 1px solid #737373;
   border-radius: 5px;
   text-align: center;
   font-size: 16px;
+  margin-bottom: 10px;
 }
 
+.image-carousel {
+  display: flex;
+  align-items: center;
+}
 
+.thumbnail-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  margin-right: 20px;
+  gap: 10px;
+}
+
+.small-image {
+  width: 80px;
+  height: auto;
+  cursor: pointer;
+  transition: opacity 0.2s;
+  opacity: 0.4;
+}
+
+.small-image:hover {
+  opacity: 1;
+}
+
+.big-image-container {
+  flex: 1;
+  text-align: center;
+}
+
+.big-image {
+  /* max-width: 100%;
+  max-height: 400px; */
+
+  width: 400px;
+}
 </style>
-
